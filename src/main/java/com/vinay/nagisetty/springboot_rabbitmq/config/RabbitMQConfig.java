ae8fa0a0f6048aadea632027103d431e0179d2a1
vinay <vinaynagisetty7@gmail.com>
2025-01-23 14:20:23 +0530

    change the config class for accept the json format by rabbigtmq

--- src/main/java/com/vinay/nagisetty/springboot_rabbitmq/config/RabbitMQConfig.java
+++ src/main/java/com/vinay/nagisetty/springboot_rabbitmq/config/RabbitMQConfig.java
@@ -1,9 +1,10 @@
 package com.vinay.nagisetty.springboot_rabbitmq.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,14 +15,25 @@ public class RabbitMQConfig {
     @Value("${rabbitmq.queue.name}")
     private  String QUEUE_NAME ;
 
+    @Value("${rabbitmq.json.queue.name}")
+    private  String Json_QUEUE_NAME ;
+
     @Value("${rabbitmq.exchange.name}")
     private  String EXCHANGE_NAME ;
 
     @Value("${rabbitmq.routing.key}")
     private  String ROUTING_KEY ;
 
+    @Value("${rabbitmq.json.routing.key}")
+    private  String json_ROUTING_KEY ;
+
     @Bean
     public Queue queue() {
+        return new Queue(QUEUE_NAME);
+    }
+
+    @Bean
+    public Queue json_queue() {
         return new Queue(QUEUE_NAME);
     }
 
@@ -35,6 +47,32 @@ public class RabbitMQConfig {
         return BindingBuilder.bind(queue())
                 .to(exchange())
                 .with(ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding json_binding() {
+        return BindingBuilder.bind(json_queue())
+                .to(exchange())
+                .with(json_ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+
+//    Connection Factory
+//     rabbit template
+//     rabbit admin
+
+//Implement the rabbitTemplate bean
+
+     @Bean
+    public AmqpTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
     }
 
 
---
+++ src/main/java/com/vinay/nagisetty/springboot_rabbitmq/dto/User.java
@@ -0,0 +1,11 @@
+package com.vinay.nagisetty.springboot_rabbitmq.dto;
+
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String name;
+    private String department;
+    private int age;
+}
